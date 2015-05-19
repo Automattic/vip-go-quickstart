7702081502486260c4f1cc77a574b5fecd3b7f29
Josh Betz <j@joshbetz.com>
2015-05-19 16:39:42 -0500

    Intial Puppet script

---
+++ puppet/default.pp
@@ -0,0 +1,11 @@
+Exec { path => '/bin:/usr/bin:/usr/local/bin:/usr/sbin:/sbin' }
+
+import 'manifests/*.pp'
+
+include apt
+include apt::update
+include apt::backports
+
+Class['apt::update'] -> Package <||>
+
+host { 'vip.local': ip => '127.0.0.1' }

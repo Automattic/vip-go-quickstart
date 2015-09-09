e4d8cbab1b5d6ab91013b59094e502daf5e366ec
Philip John <phil@philipjohn.co.uk>
2015-09-09 14:00:34 +0100

    Avoid printing out the whole commands during provisioning, add some explanatory comments

--- puppet/manifests/wordpress.pp
+++ puppet/manifests/wordpress.pp
@@ -1,9 +1,13 @@
-exec { '/usr/bin/wp --allow-root core download --path="/var/www"':
+# Download WordPRess
+exec { 'wp_core_download':
+    command => '/usr/bin/wp --allow-root core download --path="/var/www"',
 	cwd     => '/var/www',
 	creates => '/var/www/wp-load.php',
 	notify  => File['/var/www'],
 	user    => 'root',
 } ->
+
+// Get our MU Plugins
 exec { 'git clone mu-plugins':
 	command => 'git clone https://github.com/Automattic/vip-mu-plugins-public.git mu-plugins',
 	cwd     => '/var/www/wp-content',
@@ -29,10 +33,14 @@ PHP',
 	user    => 'root',
 } ->
 
-exec { 'find /var/www -not -path "*wp-content/themes*" -not -path "*wp-content/plugins*" -print0 | xargs -0 /bin/chown www-data:www-data':
+# Set the correct owner/group for WP core files
+exec { 'chown':
+    command: 'find /var/www -not -path "*wp-content/themes*" -not -path "*wp-content/plugins*" -print0 | xargs -0 /bin/chown www-data:www-data',
 	cwd	=> '/var/www',
 	user	=> 'root',
 } ->
+
+# Run the WordPress install
 wp::site { '/var/www':
 	url            => 'http://vip.local',
 	name           => 'VIP',
@@ -40,19 +48,24 @@ wp::site { '/var/www':
 	admin_password => 'wordpress',
 	require        => Mysql::Db['wordpress']
 } ->
-exec { '/usr/bin/wp --allow-root plugin delete hello':
-        cwd     => '/var/www',
-        user    => 'root',
+
+# Remove unnecessary plugins
+exec { 'remove_hello_dolly':
+    command => '/usr/bin/wp --allow-root plugin delete hello',
+    cwd     => '/var/www',
+    user    => 'root'
 } ->
-exec { '/usr/bin/wp --allow-root plugin delete akismet':
-        cwd     => '/var/www',
-        user    => 'root'
+exec { 'remove_akismet':
+    command => '/usr/bin/wp --allow-root plugin delete akismet',
+    cwd     => '/var/www',
+    user    => 'root'
 }
 
 # TODO: Update to latest stable
 # TODO: Add mu-plugins
 # TODO: Add debug plugins
 
+# Check the owner/group permissions are correct
 file { '/var/www':
 	ensure  => 'directory',
 	owner   => 'www-data',

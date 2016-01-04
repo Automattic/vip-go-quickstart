74a0e50be65a29b034925b7f21d78c3976fdb8dc
Simon Wheatley <simon@simonwheatley.co.uk>
2016-01-04 17:28:51 +0000

    Set PHP `error_log` config value to `/var/log/php/error.log`
    This Puppet change: creates the directory for the log file,
    creates the log file with the right owner and permissions,
    sets PHP to log to the error log file.
    
    Addresses #28

--- puppet/manifests/php.pp
+++ puppet/manifests/php.pp
@@ -2,4 +2,23 @@ class { 'php':
 	service => 'nginx',
 }
 
-php::module { ['fpm','cli','mysql']: }
+service { 'php5-fpm':
+    ensure => running,
+    enable => true,
+}
+
+php::module { ['fpm','cli','mysql']: } ->
+file { '/var/log/php/' :
+  ensure => directory,
+  owner => www-data, group => www-data, mode => 444,
+} ->
+file { '/var/log/php/error.log' :
+  ensure => present,
+  owner => www-data, group => www-data, mode => 644,
+} ->
+file { '/etc/php5/fpm/conf.d/error.ini' :
+  ensure => present,
+  notify  => Service['php5-fpm'],
+  owner => root, group => root, mode => 644,
+  content => "error_log = /var/log/php/error.log\n",
+}

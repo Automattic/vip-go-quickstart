b9e1d1ea3597cfde90f1a924e9439ba3db7215ab
Josh Betz <j@joshbetz.com>
2016-06-13 14:36:17 -0500

    Upgrade to PHP 7 ahead of production.
    Upgrade Quickstart to PHP 7 ahead of production so devs can
    test in their local environments.

--- puppet/default.pp
+++ puppet/default.pp
@@ -6,7 +6,17 @@ include apt
 include apt::update
 include apt::backports
 
-Class['apt::update'] -> Package <||>
+# Make sure apt-get is up-to-date before we do anything else
+stage { 'updates': before => Stage['main'] }
+class { 'updates': stage  => updates }
+
+# updates
+class updates {
+	exec { 'apt-get update':
+		command   => 'apt-get update --quiet --yes',
+		timeout => 0
+	}
+}
 
 host { 'vip-go':
     name => $hostname,
--- puppet/manifests/php.pp
+++ puppet/manifests/php.pp
@@ -1,13 +1,19 @@
+apt::ppa { 'ppa:ondrej/php': } ->
 class { 'php':
-	service => 'nginx',
+	package     => 'php7.0',
+	service     => 'nginx',
+	config_dir  => '/etc/php/7.0',
+	config_file => '/etc/php/7.0/apache2/php.ini',
 }
 
-service { 'php5-fpm':
+service { 'php7.0-fpm':
     ensure => running,
     enable => true,
 }
 
-php::module { ['fpm','cli','mysql']: } ->
+php::module { ['fpm','cli','mysql']:
+	module_prefix => 'php7.0-'
+} ->
 file { '/var/log/php/' :
   ensure => directory,
   owner => www-data, group => www-data, mode => 444,
@@ -16,9 +22,9 @@ file { '/var/log/php/error.log' :
   ensure => present,
   owner => www-data, group => www-data, mode => 644,
 } ->
-file { '/etc/php5/fpm/conf.d/error.ini' :
+file { '/etc/php/7.0/fpm/conf.d/error.ini' :
   ensure => present,
-  notify  => Service['php5-fpm'],
+  notify  => Service['php7.0-fpm'],
   owner => root, group => root, mode => 644,
   content => "error_log = /var/log/php/error.log\n",
 }
